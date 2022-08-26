6b6e9f73edbeb9b417875990c14078b57956fe7d
Ulisses Silva <ulissesrdasilva@gmail.com>
2022-08-26 12:45:37 -0300

    fix: cors

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { configure as serverlessExpress } from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import cors from 'cors';
 
 let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+
+  const options: cors.CorsOptions = {
+    methods: 'GET,POST,OPTIONS',
+    origin: '*',
+  };
+
+  app.use(cors(options));
+
+  app.use(helmet());
+
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
